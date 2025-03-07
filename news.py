3aec69e58b2d648d45549b62cc4c408c633e17c1
FairozAhmadSheikh <fairozahmadsheikh@gmail.com>
2025-03-07 14:47:08 +0530

    gaurdian_bug

--- news.py
+++ news.py
@@ -10,10 +10,12 @@ def fetch_news():
         return
     
     soup = BeautifulSoup(response.text, "html.parser")
-    articles = soup.find_all("h3", class_="fc-item__title", limit=10)
+    
+    # Find article sections
+    articles = soup.find_all("a", class_="u-faux-block-link__overlay", limit=10)
 
     for article in articles:
-        print(article.text.strip(), "-", article.a["href"])
+        print(article.text.strip(), "-", article["href"])
 
 if __name__ == "__main__":
     fetch_news()
