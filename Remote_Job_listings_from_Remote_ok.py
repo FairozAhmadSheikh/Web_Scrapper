675289f1d10b03351bf07109b7c5bda975a19bff
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-05-08 08:12:55 +0530

    c2

--- Remote_Job_listings_from_Remote_ok.py
+++ Remote_Job_listings_from_Remote_ok.py
@@ -3,3 +3,17 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import time
+
+# URL of the site
+url = "https://remoteok.com/remote-dev-jobs"
+
+# Custom headers to mimic a browser
+headers = {
+    "User-Agent": "Mozilla/5.0"
+}
+
+# Send GET request
+response = requests.get(url, headers=headers)
+
+# Parse HTML
+soup = BeautifulSoup(response.content, "html.parser")
